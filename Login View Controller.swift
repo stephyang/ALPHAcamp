b2fe1a7ba52f8ddb55bb3da080dd7d92d1125481
stephyang <stephanie1635945@gmail.com>
2016-09-25 10:52:07 +0800

    login Features

--- Login View Controller.swift
+++ Login View Controller.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Alamofire
+import SwiftyJSON
 
 class Login_View_Controller: UIViewController {
     @IBOutlet weak var textFieldEmail: UITextField!
@@ -26,16 +27,42 @@ class Login_View_Controller: UIViewController {
     
     @IBAction func doLogin(sender: AnyObject) {
         let email = textFieldEmail.text
-        let password = textFieldEmail.text
+        let password = textFieldPassword.text
+        let url = "https://dojo.alphacamp.co/api/v1/login"
         
-        let url = "https://dojo.alphacamp.co/api/vi/login"
+        print("hi")
         
-        Alamofire.request(.POST, url, parameters: ["email": eamil!,"password":password!] encoding: <#T##ParameterEncoding#>, headers: <#T##[String : String]?#>)
+        Alamofire.request(.POST, url, parameters: ["email": email!, "password":password!,"api_key":"ef81632058bf2e75925f7f9d747a34df55d86072"]).responseJSON
+            { response in
+               
+                if JSON(response.result.value!)["message"].stringValue == "Ok"{
+                let auth_token = JSON(response.result.value!)["auth_token"].stringValue
+//                    寫入userDefault
+                let userDefault = NSUserDefaults.standardUserDefaults()
+                    userDefault.setObject(auth_token, forKey: "auth_token")
+                    userDefault.synchronize()
+
+                    self.dismissViewControllerAnimated(true, completion: nil)
+
+                }
+                
+                if JSON(response.result.value!)["message"].description == "Failed"{
+                    let alert = UIAlertController.init(title: nil, message: "帳號或密碼有誤，請再輸入一次", preferredStyle: .Alert)
+                    let okAction = UIAlertAction(title: "OK", style: .Cancel, handler: nil)
+                    alert.addAction(okAction)
+                    
+                    self.presentViewController(alert, animated: true, completion: nil)
+                    
+                
+                }
+                            
+        }
+        
+        
     }
 
 
-    
-    
+
     
     
 }
